0255f64bee68446cc7c3453bc2f3ceb0fa822ac4
Arthur Millet <arthur.millet@etu.univ-grenoble-alpes.fr>
2023-12-01 16:25:41 +0100

    Ajout des fonctions pour aligner

--- mem.c
+++ mem.c
@@ -63,6 +63,14 @@ static inline size_t get_system_memory_size() {
 	return get_header()->memory_size;
 }
 
+static inline size_t aligne_taille(size_t taille, int alignement){
+	return taille+alignement-(taille%alignement);
+}
+
+static inline void* aligne_adresse(void* adresse, int alignement){
+	return (void*)0;
+}
+
 void mem_init(void *mem, size_t taille) {
 	memory_addr = mem;
 	/* On vérifie qu'on a bien enregistré les infos et qu'on
@@ -75,9 +83,6 @@ void mem_init(void *mem, size_t taille) {
 
 	struct zones_libres* l;
 	l = memory_addr + sizeof(struct allocator_header);
-	if (l%16 != 0){
-		l = l+16-l%16;
-	}
 	l->size = taille - sizeof(struct allocator_header);
 	l->next = NULL;
 
@@ -128,7 +133,7 @@ void *mem_alloc(size_t taille) {
 	if (zones_libres == NULL){
 		return NULL;
 	}
-	return &(zones_libres);
+	return (void*)zones_libres;
 
 }
 
